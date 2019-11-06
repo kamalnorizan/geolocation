c387f29f289bf19abf89db35861b3144545234d0
Kamal Norizan <kamalnorizan@gmail.com>
2019-11-06 21:36:13 +0800

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,47 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private geolocation: Geolocation
+  ) { }
+  longitud: any;
+  latitude: any;
+  disabled = 'disabled';
+  jarak: any;
+  ionViewWillEnter() {
+    this.geolocation.getCurrentPosition().then(
+      position => {
+        this.longitud = position.coords.longitude;
+        this.latitude = position.coords.latitude;
+        this.jarak = this.distance(this.latitude, this.longitud);
+        if (this.jarak < 100) {
+          console.log(this.jarak);
+          this.disabled = 'false';
+        }
+      }
+    );
+  }
 
+  toRadius(value) {
+    return value * Math.PI / 180;
+  }
+
+  distance(lat1, lon1) {
+    let lat2 = 4.210643; // dari db
+    const lon2 = 101.975766; // dari db
+    const R = 6371;
+    const distLat = this.toRadius(lat2 - lat1);
+    const distLon = this.toRadius(lon2 - lon1);
+    lat1 = this.toRadius(lat1);
+    lat2 = this.toRadius(lat2);
+
+    const a = Math.sin(distLat / 2) * Math.sin(distLat / 2) + Math.sin(distLon / 2) * Math.sin(distLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const dist = R * c;
+
+    return dist;
+  }
+  test() {
+    alert('done');
+  }
 }
